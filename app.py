77016589d04cd9ade4fb4d6e3fbf3a52bead81a3
Arif Cengic <acengic_c@ngs.org>
2020-04-13 22:47:39 -0400

    updated

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 # app.py
 
 from flask import Flask
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
 app = Flask(__name__)
+
+AUTH_TOKEN = os.environ("AUTH_TOKEN")
+ACCOUNT_SID = os.environ("ACCOUNT_SID")
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/other")
-def other():
-    return "Hello Other World!"
+@app.route("/sms/<phone>/<message>")
+def send_sms(phone, message):
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages \
+            .create(
+                body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                # messaging_service_sid='MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                from_='+12082739639',
+                to='+12027511880'
+            )
+
+    print(message.sid)
+    return f"Message ${phone} Hello ${message}!"
